21d6d99f2dfcf5446d1c236ec9f93133054b309f
zchatfield <chatfield.z.c@gmail.com>
2022-09-29 22:27:05 -0400

    Completed Task 5. Conditional Statement

--- javascript/external.js
+++ javascript/external.js
@@ -13,9 +13,23 @@ sign = window.prompt(); // open the blank prompt window
 sign = prompt();       //  open the blank prompt window
 sign = window.prompt('Are you feeling lucky'); // open the window with Text "Are you feeling lucky"
 sign = window.prompt('Are you feeling lucky', 'sure'); // open the window with Text "Are you feeling lucky" and default value "sure"
-*/
 x = 22;
-const y =33;
+y =33;
 document.writeln(x + y)  ;
 document.writeln("<br>");
 document.writeln(x += 33);
+*/
+document.writeln("<button onclick='condition()'>Conditional Test</button>")
+
+function condition()
+{
+  x = confirm("Are you sure you want to proceed?");
+  if(x)
+  {
+    document.writeln("You chose Okay!");
+     }
+     else
+     {
+       document.writeln("You chose Cancel!");
+     }
+   }
