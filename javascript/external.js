7abe704b019ab6443bd626ee785a330794151747
zchatfield <chatfield.z.c@gmail.com>
2022-09-29 22:13:24 -0400

    Completed Task 4. Operators

--- javascript/external.js
+++ javascript/external.js
@@ -3,7 +3,7 @@ const y = "mapping";
 const out = x + y;
 document.write(out);
 */
-
+/*
 //A prompt box is used to prompt users to input a value before entering a page.
 user_name = window.prompt("Please enter your name","Type your name here");
 document.write(user_name);
@@ -13,3 +13,9 @@ sign = window.prompt(); // open the blank prompt window
 sign = prompt();       //  open the blank prompt window
 sign = window.prompt('Are you feeling lucky'); // open the window with Text "Are you feeling lucky"
 sign = window.prompt('Are you feeling lucky', 'sure'); // open the window with Text "Are you feeling lucky" and default value "sure"
+*/
+x = 22;
+const y =33;
+document.writeln(x + y)  ;
+document.writeln("<br>");
+document.writeln(x += 33);
