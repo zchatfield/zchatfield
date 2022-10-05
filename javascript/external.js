023ea13995e7a44d30b286922c8855c695d26fd9
zchatfield <chatfield.z.c@gmail.com>
2022-10-04 21:06:00 -0400

    Defined a function for the welcome message

--- javascript/external.js
+++ javascript/external.js
@@ -1,5 +1,24 @@
 
 //TASK 2.1 + .2 mod
 
-user_name = window.prompt("What's your name?", "Type your name here");
-document.write(' '+user_name+'!');
+/*user_name = window.prompt("What's your name?", "Type your name here");
+document.write(' '+user_name+'!');*/
+
+//LAB 4
+function welcome()
+
+{
+
+let a = "Please enter your name.";
+let b = "Type your name here.";
+
+// A prompt box is used to prompt users to input a value before entering a page.
+user_name = window.prompt(a, b);
+
+message = "<hi>Hello, welcome to my webpage, " + user_name + "!</h1>"
+
+return message
+
+}
+
+document .write(welcome());
