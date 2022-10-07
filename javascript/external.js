22386e8d5fc8de4831796dfeccce1bc1f8cfcfd2
zchatfield <chatfield.z.c@gmail.com>
2022-10-07 16:18:23 -0400

    Filled content from web maps array into the web maps table.

--- javascript/external.js
+++ javascript/external.js
@@ -5,10 +5,11 @@
 document.write(' '+user_name+'!');*/
 
 //LAB 4
+/*
 var webmaps =
 [
   ["Oil Spill Toolkit",
-    "https://www.glo.texas.gov/ost/-,
+    "https://www.glo.texas.gov/ost/-,"
     "The Oil Spill Toolkit developed by Enterprise Technology Solutions"
     ],
   ["Texas Ecosystems Analytical Mapper",
@@ -16,12 +17,22 @@ var webmaps =
     "The Texas Parks and Wildlife's Landscape Ecology program developed"
     ]
 ];
+*/
 
+var webmaps =
+[
+  ["Google Earth",
+    "https://earth.google.com/",
+    "GoogleEarth has several functions available on it, but I think the primary purpose for it is for projects or planning that involves multiple places or a span of space in an area."
+  ],
+  ["ArcGis",
+    "https://www.arcgis.com/apps/mapviewer/index.html",
+    "In some ways, the user experience of this platform is similar to GoogleEarth. However, there are far more functional options, and they’re all geared specifically toward spatial analysis, data visualization, and uses that are not for the general public but instead for those who are trained in GIS.",
+  ]
+]
 
 function welcome()
-
 {
-
 let a = "Please enter your name.";
 let b = "Type your name here.";
 
@@ -31,29 +42,17 @@ user_name = window.prompt(a, b);
 message = "<hi>Hello, welcome to my webpage, " + user_name + "!</h1>"
 
 return message
-
 }
-/*
-function webmap_table(){
+
+function webmap_table()
+{
   document.write("<table width=100%>")
-  for (var row = 0; row < webmaps.length; row++) {
+  for(var row = 0; row < webmaps.length; row++){
     document.write("<tr>");
-  for(var column = 0; column < webmaps[0].length; column++){
-      document.write("<td>"+webmaps[row][column]+"</td>");
-    }
+      for(var column = 0; column < webmaps[0].length; column++){
+        document.write("<td>"+webmaps[row][column]+"</td>");
+        }
   document.write('</tr>')
   }
 document.write('</table>')
-} */
-
-function webmap_table(){
-    document.write("<table width=100%>")
-  for(var row = 0; row <  webmaps.length; row++){
-        document.write("<tr>");
-    for(var column = 0; column < webmaps[0].length; column++){
-          document.write("<td>"+webmaps[row][column]+"</td>");
-        }
-  document.write('</tr>')
-    }
-  document.write('</table>')
 }
